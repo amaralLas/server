2c4d7ce2821a6d7e049e673e0247d6c8f798a540
amaralstreaming <leonardo.amaral.sousa@gmail.com>
2015-10-29 13:33:44 -0300

    teste

--- src/br/com/dao/UsuarioDAO.java
+++ src/br/com/dao/UsuarioDAO.java
@@ -1,11 +1,57 @@
 package br.com.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import br.com.pojo.Usuario;
+import br.com.util.JPAUtils;
 
 public class UsuarioDAO {
 
-	public boolean insert(Usuario usuario){
-		
-		return true;
+	public void insert(Usuario usuario){
+		EntityManager manager = JPAUtils.getFactory().createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(usuario);
+		manager.getTransaction().commit();
+		manager.close();
 	}
+	
+	public void delete(Usuario usuario){
+		EntityManager manager = JPAUtils.getFactory().createEntityManager();
+		manager.getTransaction().begin();
+	    manager.remove(usuario);
+		manager.getTransaction().commit();
+		manager.close();
+	}
+	
+	public void update(Usuario usuario){
+		EntityManager manager = JPAUtils.getFactory().createEntityManager();
+		manager.getTransaction().begin();
+	    manager.merge(usuario);
+		manager.getTransaction().commit();
+		manager.close();
+	}
+	
+	public Usuario getId(int idUsuario){
+		EntityManager manager = JPAUtils.getFactory().createEntityManager();
+		manager.getTransaction().begin();
+	    Usuario retorno = manager.find(Usuario.class, idUsuario);
+		manager.getTransaction().commit();
+		manager.close();
+		return retorno;
+	}
+	
+	public List<Usuario> getAll(){
+		EntityManager manager = JPAUtils.getFactory().createEntityManager();
+		manager.getTransaction().begin();
+		List<Usuario> usuarios;
+	    Query query= manager.createQuery("SELECT u FROM ead_ususario u", Usuario.class);
+	    usuarios = query.getResultList();
+		manager.getTransaction().commit();
+		manager.close();
+		return usuarios;
+	}
+	
 }
